0675817387864b8d085a8f8d8a9938d724043c9a
Bhavna Mahalle <bhavna.kohale@gmail.com>
2024-07-28 19:13:00 -0700

    adding assgn 2

---
+++ assingments/Assignment_2.java
@@ -0,0 +1,36 @@
+package assingments;
+
+public class Assignment_2 {
+
+	public static void main(String[] args) {
+	//write a program to perform addition ,sub,mul,div and modus in the main method
+		// multiple program in perticular main method
+		
+		int a = 25;
+		int b = 25;
+		int add = a + b ;
+		System.out.println(add);
+			
+		int a1 = 25;
+		int b1 = 25;
+		int sub = a1 - b1 ;
+		System.out.println(sub);
+		
+		int a2 = 25;
+		int b2 = 25;
+		int mul = a2 * b2 ;
+		System.out.println(mul);
+			
+		int a3 = 25;
+		int b3 = 25;
+		int div = a3 / b3 ;
+		System.out.println(div);
+			
+		int a4 = 25;
+		int b4 = 25;
+		int modulus = a4 % b4 ;
+		System.out.println(modulus);
+		
+	}
+
+}
